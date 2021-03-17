fc238dab5e29866a9b24fe8da8d9c53596468590
Sebastien Gauthier <44874281+sebgauthier@users.noreply.github.com>
2021-03-17 10:57:52 -0400

    form @submit (#1)
    * form @submit
    
    * removed console log

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,72 +1,69 @@
 <template>
   <div>
-      <h2>Please Log In</h2>
-      <form>
-        <label>Username: </label>
-        <input type="text" v-model="username"/>
-        <label>Password: </label>
-        <input type="password" v-model="password"/>
-        <button type="button" @click.prevent="performLogin">Sign in</button>
-      </form>
+    <h2>Please Log In</h2>
+    <form @submit.prevent="performLogin">
+      <label>Username: </label>
+      <input type="text" v-model="username" />
+      <label>Password: </label>
+      <input type="password" v-model="password" />
+      <button type="submit">Sign in</button>
+    </form>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
-import crypto   from 'crypto';
+import crypto from 'crypto'
 import axios from '../composables/axiosClient'
-import { useUser } from '../composables/useUser';
+import { useUser } from '../composables/useUser'
 import { useRouter } from 'vue-router'
 
 export default {
-    setup() {
-        const username = ref('');
-        const password = ref('');
-        const router = useRouter();
+  setup() {
+    const username = ref('')
+    const password = ref('')
+    const router = useRouter()
 
-        const performLogin = async () => {
-            // Post to /login to get salt for user
-            try {
-                let userId = username.value;
-                let res = await axios.post( '/login', { userId } );
-                if( res.status != 200 ){
-                    console.log('Failed to retrieve salt');
-                    return;
-                }
-
-                // Hash password and send pass for user to /login
-                const hash = crypto.createHash('sha256');
-                hash.update( res.data.salt + password.value );
-                const pwHash = hash.digest().toString('hex');
-                res = await axios.post('/login', { pwHash, userId } )
-
-                if( res.status != 200 ){
-                    console.log('Failed to log in.');
-                    return;
-                }
-
-                // Login was successful... login
-                var user = useUser();
-                user.login({ sampledata: 'test'},res.data.auth_token );
-
-                console.log('Logged In.');
-                user = useUser();
-                router.push({ name: 'Home' });
-            }catch( err ){
-                console.log(err);
-            }
-
+    const performLogin = async () => {
+      // Post to /login to get salt for user
+      try {
+        let userId = username.value
+        let res = await axios.post('/login', { userId })
+        if (res.status != 200) {
+          console.log('Failed to retrieve salt')
+          return
         }
 
-        return {
-            username,
-            password,
-            performLogin,
+        // Hash password and send pass for user to /login
+        const hash = crypto.createHash('sha256')
+        hash.update(res.data.salt + password.value)
+        const pwHash = hash.digest().toString('hex')
+        res = await axios.post('/login', { pwHash, userId })
+
+        if (res.status != 200) {
+          console.log('Failed to log in.')
+          return
         }
+
+        // Login was successful... login
+        var user = useUser()
+        user.login({ sampledata: 'test' }, res.data.auth_token)
+
+        console.log('Logged In.')
+        user = useUser()
+        router.push({ name: 'Home' })
+      } catch (err) {
+        console.log(err)
+      }
     }
+
+    return {
+      username,
+      password,
+      performLogin,
+    }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
